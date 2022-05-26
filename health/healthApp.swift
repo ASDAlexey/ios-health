f5f76fecff7ce3104b87e1f3eaf04399c07f2bc7
alexey popov <alexey.popov@5thkind.com>
2022-05-26 19:45:42 +0300

    Initial Commit

---
+++ health/healthApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthApp.swift
+//  health
+//
+//  Created by Alexey on 26.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct healthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
